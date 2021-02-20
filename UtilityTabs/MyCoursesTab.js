067b55634cdaabff87447fd408c0e25a676565f5
jmutton2 <55411119+jmutton2@users.noreply.github.com>
2021-02-20 18:40:43 -0500

    Revert "profile update, coursePage"

--- UtilityTabs/MyCoursesTab.js
+++ UtilityTabs/MyCoursesTab.js
@@ -1,59 +1,14 @@
 import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as ReactBootStrap from "react-bootstrap";
+import { View, Text } from 'react-native';
 
-const myCourses = () => {
-
-      
-
-
-    const players =[
-      {CourseName: "Calculus", CourseCode: "APPLMATH 2276", Prof: "Bob"},
-      {CourseName: "Geography", CourseCode: "GEOGRAPH 2132", Prof: "Tom"},
-      {CourseName: "Electrical Engineering", CourseCode: "ECE 2238", Prof: "Luke"},
-      {CourseName: "Software Design", CourseCode: "SE 2203", Prof: "Bill"},
-    ]
-
-    const renderTable =(course,index) => {
-      return(
-        <tr key={index}>
-          <td>{course.CourseName}</td>
-          <td>{course.CourseCode}</td>
-          <td>{course.Prof}</td>
-        </tr>
-      )
-    }
-
-
+function myCourses() {
     return (
-      <div className="myCourses">
-        <ReactBootStrap.Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Course Name</th>
-            <th>Course Code</th>
-            <th>Prof</th>
-          </tr>
-        </thead>
-        <tbody>
-          {players.map(renderTable)}
-        </tbody>
-        </ReactBootStrap.Table>
-      </div>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>List view of my courses</Text>
+      </View>
     )
-    
-
-
 };
 
 export default myCourses;
 
-const styles = StyleSheet.create({
-  butt: {
-    marginTop: '70%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
 
--- UtilityTabs/SettingsTab.js
+++ UtilityTabs/SettingsTab.js
@@ -1,23 +1,12 @@
 import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
 function Settings() {
     return (
-      <View style={styles.butt}>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-    </View>
+      <View>
+        <Text>settings</Text>
+      </View>
     )
 }
-
-const styles = StyleSheet.create({
-  butt: {
-    marginTop: '70%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
 
 export default Settings;
