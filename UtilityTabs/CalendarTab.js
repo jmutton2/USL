2b3b614de4cf18b0658d760543cb641e09bb2e93
jmutton2 <jmutton2@uwo.ca>
2021-02-20 06:55:14 -0500

    xx

--- UtilityTabs/CalendarTab.js
+++ UtilityTabs/CalendarTab.js
@@ -1,62 +1,114 @@
-import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import React, {Component} from 'react';
+import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Agenda} from 'react-native-calendars';
 
-export default function calendar() {
-  return (
-    <View style={styles.container}>
+const testIDs = require('../assets/testIDs.js');
+
+export default class calendar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: {}
+    };
+  }
+
+  render() {
+    return (
       <Agenda
-
-        // Callback that gets called when items for a certain month should be loaded (month became visible)
-        loadItemsForMonth={(month) => {console.log('trigger items loading')}}
-        // Callback that fires when the calendar is opened or closed
-        onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
-        // Callback that gets called on day press
-        onDayPress={(day)=>{console.log('day pressed')}}
-        // Callback that gets called when day changes while scrolling agenda list
-        onDayChange={(day)=>{console.log('day changed')}}
-        // Initially selected day
-        //selected={'2012-05-16'}
-        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-        minDate={'2012-05-10'}
-        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        maxDate={'2012-05-30'}
-        // Max amount of months allowed to scroll to the past. Default = 50
-        pastScrollRange={50}
-        // Max amount of months allowed to scroll to the future. Default = 50
-        futureScrollRange={50}
-        // Specify how each item should be rendered in agenda
-        renderItem={(item, firstItemInDay) => {return (<View />);}}
-        // Specify how each date should be rendered. day can be undefined if the item is not first in that day.
-        renderDay={(day, item) => {return (<View />);}}
-        // Specify how empty date content with no items should be rendered
-        renderEmptyDate={() => {return (<View />);}}
-        // Specify what should be rendered instead of ActivityIndicator
-        renderEmptyData = {() => {return (<View />);}}
-        // Specify your item comparison function for increased performance
-        rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
-
-        // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
-        // disabledByDefault={true}
-        // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-        // onRefresh={() => console.log('refreshing...')}
-        // Set this true while waiting for new data from a refresh
-        // refreshing={false}
-        // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-        // refreshControl={null}
-
+        testID={testIDs.agenda.CONTAINER}
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2017-05-16'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#43515c'},
+        //    '2017-05-09': {textColor: '#43515c'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+        // hideExtraDays={false}
       />
-    </View>
-  )
+    );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {
+        newItems[key] = this.state.items[key];
+      });
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity
+        testID={testIDs.agenda.ITEM}
+        style={[styles.item, {height: item.height}]}
+        onPress={() => Alert.alert(item.name)}
+      >
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  item: {
+    backgroundColor: 'white',
     flex: 1,
-    justifyContent: 'center',
-    marginTop: 50,
-    height: 500
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30
   }
-})
-
-
+});
